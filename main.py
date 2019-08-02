75ec12154838a019a2cdbed119af65ed356f2de4
Arthuchaut <arthur.audebrand@afnor.org>
2019-08-02 14:11:00 +0200

    Initialize the project structure

---
+++ main.py
@@ -0,0 +1,16 @@
+"""The main file of the project.
+
+It contains the main function for lauching the Slack bot.
+"""
+
+def main() -> int:
+    """The entry point of the project.
+    """
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main())
